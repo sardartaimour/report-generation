debb22a6c6532a25dae037735c90c40dda9745b2
taimourali <sardartaimour786@gmail.com>
2025-01-28 09:56:22 +0500

    fixes

--- src/app/features/report-generation/summary-page/summary-page.component.ts
+++ src/app/features/report-generation/summary-page/summary-page.component.ts
@@ -6,11 +6,19 @@ import { VisitorSummaryComponent } from './visitor-summary/visitor-summary.compo
 import { MaterialSummaryComponent } from './material-summary/material-summary.component';
 import { DeliverySummaryComponent } from './delivery-summary/delivery-summary.component';
 import { AttendanceSummaryComponent } from './attendance-summary/attendance-summary.component';
+import { WorkProgressSummaryComponent } from './work-progress-summary/work-progress-summary.component';
 
 @Component({
   selector: 'app-summary-page',
   standalone: true,
-  imports: [CommonModule, AttendanceSummaryComponent, VisitorSummaryComponent, MaterialSummaryComponent, DeliverySummaryComponent],
+  imports: [
+    CommonModule, 
+    AttendanceSummaryComponent, 
+    VisitorSummaryComponent, 
+    MaterialSummaryComponent, 
+    DeliverySummaryComponent,
+    WorkProgressSummaryComponent
+  ],
   templateUrl: './summary-page.component.html',
   styleUrl: './summary-page.component.scss'
 })
